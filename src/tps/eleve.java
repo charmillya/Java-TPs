620ca891df08fa386af7953ee52217a9c19f7b22
Titouan WISSER <titouanwisser@gmail.com>
2025-09-17 10:58:30 +0200

    TP Entrée + élève

---
+++ src/tps/eleve.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tps;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Titouan
+ */
+class eleve {
+
+    private String nom;
+    private double noteOral;
+    private double noteEcrit;
+    
+    private static int nbEleves;
+    
+    public void initial(String nom, double noteOral, double noteEcrit) {
+        this.nom = nom;
+        this.noteOral = noteOral;
+        this.noteEcrit = noteEcrit;
+        nbEleves++;
+    }
+    
+    private double moyenne() {
+        return (2 * noteEcrit + noteOral) / 3;
+    }
+    
+    public void affiche() {
+        System.out.println(this.nom + " : " + this.moyenne());
+    }
+    
+    public static int getNbEleves() {
+        return nbEleves;
+    }
+    
+    public static eleve meilleur(eleve[] tabEleves) {
+        var meilleur = new eleve();
+        for(int i = 0; i < tabEleves.length; i++) {
+            if(tabEleves[i].moyenne() > meilleur.moyenne()) {
+                meilleur = tabEleves[i];
+            }
+        }
+        return meilleur;
+    }
+    
+    
+    public static void main(String[] args) throws IOException {
+        
+        eleve[] tabEleves = new eleve[5];
+        for(var i = 0; i < tabEleves.length; i++) {
+            tabEleves[i] = new eleve();
+            tabEleves[i].initial("Eleve " + (i+1), (i+1)*2.8, i*4.5);
+            tabEleves[i].affiche();
+        }
+        
+        System.out.println(getNbEleves());
+        System.out.println("Meilleur élève : ");
+        meilleur(tabEleves).affiche();
+        
+    }
+
+}
